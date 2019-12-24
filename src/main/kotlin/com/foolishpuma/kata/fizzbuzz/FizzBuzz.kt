5705279e2a4246078444ff185b0c5673e867192d
Tony Card <tony.card@accenture.com>
2019-12-23 20:42:00 -0500

    classic refactor - extracted logic to functions.

--- src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
@@ -2,9 +2,13 @@ package com.foolishpuma.kata.fizzbuzz
 
 fun fizzBuzz(number: Int): String {
     return when {
-        number.rem(3) == 0 && number.rem(5) == 0 -> "FizzBuzz"
-        number.rem(3) == 0 -> "Fizz"
-        number.rem(5) == 0 -> "Buzz"
+        isDivisibleBy15(number) -> "FizzBuzz"
+        isDivisibleBy3(number) -> "Fizz"
+        isDivisibleBy5(number) -> "Buzz"
         else -> number.toString()
     }
 }
+
+fun isDivisibleBy3(number: Int) = number.rem(3) == 0
+fun isDivisibleBy5(number: Int) = number.rem(5) == 0
+fun isDivisibleBy15(number: Int) = isDivisibleBy3(number) && isDivisibleBy5(number)
