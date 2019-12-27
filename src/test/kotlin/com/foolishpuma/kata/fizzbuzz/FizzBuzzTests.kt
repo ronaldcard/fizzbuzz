1373a9538196c746ef15a7b7cfd4c60f17b650b0
Tony Card <tony.card@accenture.com>
2019-12-27 17:58:36 -0500

    refactored tests

--- src/test/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzzTests.kt
+++ src/test/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzzTests.kt
@@ -1,29 +1,48 @@
 package com.foolishpuma.kata.fizzbuzz
 
+import org.assertj.core.api.Assertions.anyOf
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Condition
 import org.junit.Ignore
 import org.junit.Test
+import java.lang.NumberFormatException
+import java.util.function.Predicate
+
 
 class FizzBuzzTests {
 
-    private val testRange = 1..101
+    private val testRange = IntRange(1, 101)
+    private val isFizz = Condition<String>(Predicate<String> { it == "Fizz" }, "is Fizz")
+    private val isBuzz = Condition<String>(Predicate<String> { it == "Buzz" }, "is Buzz")
+    private val isFizzBuzz = Condition<String>(Predicate<String> { it == "FizzBuzz" }, "is FizzBuzz")
+    private val isNumeric = Condition<String>(Predicate<String> { it.isNumeric() }, "is Numeric")
 
     @Test
     fun `every result is either "Fizz", "Buzz", or a numeric string`() {
+        testRange.map { number ->
+            assertThat(fizzBuzz(number)).`is`(anyOf(isFizz, isBuzz, isFizzBuzz, isNumeric))
+        }
+    }
+
+    @Test
+    fun `every numeric result corresponds to its ordinal position`() {
 
         testRange.map { number ->
             val result = fizzBuzz(number)
-            assertThat(result).isIn("Fizz", "Buzz", "FizzBuzz", result.toIntOrNull().toString())
+            if (result.isNumeric()) {
+                assertThat(result.toInt()).isEqualTo(number)
+            }
         }
     }
 
     @Ignore
     @Test
-    fun `every numeric result corresponds to its ordinal position`() { }
+    fun `every third result contains "Fizz"`() {
 
-    @Ignore
-    @Test
-    fun `every third result contains "Fizz"`() { }
+        testRange.step(3).map { number ->
+            println("[$number]")
+        }
+    }
 
     @Ignore
     @Test
@@ -45,6 +64,15 @@ class FizzBuzzTests {
     @Test
     fun `the ordinal position of every "FizzBuzz" result is divisible by 15`() {}
 
+    // TODO - is this the best way to do this?
+    private fun String.isNumeric(): Boolean {
+        return try {
+            toInt()
+            true
+        } catch (numberFormatException: NumberFormatException) {
+            false
+        }
+    }
 //    @Test
 //    fun `when passed a number, fizzBuzz should return that number`() {
 //        assertThat(fizzBuzz(1)).isEqualTo("1")
