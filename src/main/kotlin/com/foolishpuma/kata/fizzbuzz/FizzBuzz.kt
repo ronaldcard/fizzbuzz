49db20b36204a6c3a2a5135ffaa4601e303797ca
Tony Card <tony.card@accenture.com>
2019-12-28 00:21:06 -0500

    make infinite private. reordered

--- src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
@@ -1,12 +1,13 @@
 package com.foolishpuma.kata.fizzbuzz
 
-fun <T> Sequence<T>.infinite() = sequence { while (true) yieldAll(this@infinite) }
-
 private val fizzes = sequenceOf("", "", "Fizz").infinite()
 private val buzzes = sequenceOf("", "", "", "", "Buzz").infinite()
 private val numbers = generateSequence(1, Int::inc).infinite()
 private val fizzBuzzes = fizzes.zip(buzzes, String::plus).zip(numbers, wordOrNumber())
 
+fun fizzBuzz(number: Int) = fizzBuzzes.take(number).toList()[number.minus(1)]
+
+private fun <T> Sequence<T>.infinite() = sequence { while (true) yieldAll(this@infinite) }
 private fun wordOrNumber(): (String, Int) -> String {
     return fun(word: String, number: Int): String {
         return if (word.isEmpty()) {
@@ -16,5 +17,3 @@ private fun wordOrNumber(): (String, Int) -> String {
         }
     }
 }
-
-fun fizzBuzz(number: Int) = fizzBuzzes.take(number).toList()[number.minus(1)]
