8fb36992c0728ca796ca8bb39b31ce9b12b603b7
Tony Card <tony.card@accenture.com>
2019-12-28 00:17:26 -0500

    simplified by removing words

--- src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
@@ -4,9 +4,8 @@ fun <T> Sequence<T>.infinite() = sequence { while (true) yieldAll(this@infinite)
 
 private val fizzes = sequenceOf("", "", "Fizz").infinite()
 private val buzzes = sequenceOf("", "", "", "", "Buzz").infinite()
-private val words = fizzes.zip(buzzes, String::plus)
 private val numbers = generateSequence(1, Int::inc).infinite()
-private val fizzBuzzes = words.zip(numbers, wordOrNumber())
+private val fizzBuzzes = fizzes.zip(buzzes, String::plus).zip(numbers, wordOrNumber())
 
 private fun wordOrNumber(): (String, Int) -> String {
     return fun(word: String, number: Int): String {
