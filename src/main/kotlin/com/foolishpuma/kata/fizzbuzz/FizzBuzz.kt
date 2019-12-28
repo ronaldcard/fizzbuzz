58d40706baee7f9e87893b7111862029b082f0f4
Tony Card <tony.card@accenture.com>
2019-12-28 00:29:44 -0500

    reformatted

--- src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
@@ -3,7 +3,10 @@ package com.foolishpuma.kata.fizzbuzz
 private val fizzes = sequenceOf("", "", "Fizz").infinite()
 private val buzzes = sequenceOf("", "", "", "", "Buzz").infinite()
 private val numbers = generateSequence(1, Int::inc).infinite()
-private val fizzBuzzes = fizzes.zip(buzzes, String::plus).zip(numbers, wordOrNumber())
+private val fizzBuzzes =
+    fizzes
+        .zip(buzzes, String::plus)
+        .zip(numbers, wordOrNumber())
 
 fun fizzBuzz(number: Int) = fizzBuzzes.take(number).toList()[number.minus(1)]
 
