7c62e609a78ac7d1eb53a401aabec044344d460a
Tony Card <tony.card@accenture.com>
2019-12-27 20:34:32 -0500

    refactored domain functions to remove duplicate in test. follows kotlin idioms anyways

--- src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
@@ -2,13 +2,13 @@ package com.foolishpuma.kata.fizzbuzz
 
 fun fizzBuzz(number: Int): String {
     return when {
-        isDivisibleBy15(number) -> "FizzBuzz"
-        isDivisibleBy3(number) -> "Fizz"
-        isDivisibleBy5(number) -> "Buzz"
+        number.isDivisibleBy15() -> "FizzBuzz"
+        number.isDivisibleBy3() -> "Fizz"
+        number.isDivisibleBy5() -> "Buzz"
         else -> number.toString()
     }
 }
 
-fun isDivisibleBy3(number: Int) = number.rem(3) == 0
-fun isDivisibleBy5(number: Int) = number.rem(5) == 0
-fun isDivisibleBy15(number: Int) = isDivisibleBy3(number) && isDivisibleBy5(number)
+fun Int.isDivisibleBy3() = rem(3) == 0
+fun Int.isDivisibleBy5() = rem(5) == 0
+fun Int.isDivisibleBy15() = rem(15) == 0
--- src/test/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzzTests.kt
+++ src/test/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzzTests.kt
@@ -92,28 +92,24 @@ class FizzBuzzTests {
                 assertThat(fizzBuzzes.indexOf(result).ordinalPosition()).`is`(divisibleBy15())
             }
     }
+}
 
-    private fun isFizz() = Condition<String>(Predicate<String> { it == FIZZ }, "is Fizz")
-    private fun isBuzz() = Condition<String>(Predicate<String> { it == BUZZ }, "is Buzz")
-    private fun isFizzBuzz() = Condition<String>(Predicate<String> { it == FIZZ_BUZZ }, "is FizzBuzz")
-    private fun isNumeric() = Condition<String>(Predicate<String> { it.isNumeric() }, "is Numeric")
-    private fun divisibleBy3() = Condition<Int>(Predicate<Int> { it.isDivisibleBy3() }, "is divisible by 3")
-    private fun divisibleBy5() = Condition<Int>(Predicate<Int> { it.isDivisibleBy5() }, "is divisible by 5")
-    private fun divisibleBy15() = Condition<Int>(Predicate<Int> { it.isDivisibleBy15() }, "is divisible by 15")
+private fun isFizz() = Condition<String>(Predicate<String> { it == FIZZ }, "is Fizz")
+private fun isBuzz() = Condition<String>(Predicate<String> { it == BUZZ }, "is Buzz")
+private fun isFizzBuzz() = Condition<String>(Predicate<String> { it == FIZZ_BUZZ }, "is FizzBuzz")
+private fun isNumeric() = Condition<String>(Predicate<String> { it.isNumeric() }, "is Numeric")
+private fun divisibleBy3() = Condition<Int>(Predicate<Int> { it.isDivisibleBy3() }, "is divisible by 3")
+private fun divisibleBy5() = Condition<Int>(Predicate<Int> { it.isDivisibleBy5() }, "is divisible by 5")
+private fun divisibleBy15() = Condition<Int>(Predicate<Int> { it.isDivisibleBy15() }, "is divisible by 15")
 
-    private fun Int.isDivisibleBy3() = rem(3) == 0
-    private fun Int.isDivisibleBy5() = rem(5) == 0
-    private fun Int.isDivisibleBy15() = rem(15) == 0
+private fun Int.ordinalPosition() = plus(1)
 
-    private fun Int.ordinalPosition() = this + 1
-
-    // TODO - is there a better way to do this?
-    private fun String.isNumeric(): Boolean {
-        return try {
-            toInt()
-            true
-        } catch (numberFormatException: NumberFormatException) {
-            false
-        }
+// TODO - is there a better way to do this?
+private fun String.isNumeric(): Boolean {
+    return try {
+        toInt()
+        true
+    } catch (numberFormatException: NumberFormatException) {
+        false
     }
 }
