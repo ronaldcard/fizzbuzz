26849490a1ffe946a6c3d6349fd58ef382b8a5ea
Tony Card <tony.card@accenture.com>
2019-12-28 00:13:57 -0500

    extracted wordOrNumber function

--- src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/foolishpuma/kata/fizzbuzz/FizzBuzz.kt
@@ -6,13 +6,16 @@ private val fizzes = sequenceOf("", "", "Fizz").infinite()
 private val buzzes = sequenceOf("", "", "", "", "Buzz").infinite()
 private val words = fizzes.zip(buzzes, String::plus)
 private val numbers = generateSequence(1, Int::inc).infinite()
-private val fizzBuzzes =
-    words.zip(numbers) { word, number ->
-        if (word.isEmpty()) {
+private val fizzBuzzes = words.zip(numbers, wordOrNumber())
+
+private fun wordOrNumber(): (String, Int) -> String {
+    return fun(word: String, number: Int): String {
+        return if (word.isEmpty()) {
             number.toString()
         } else {
             word
         }
     }
+}
 
 fun fizzBuzz(number: Int) = fizzBuzzes.take(number).toList()[number.minus(1)]
